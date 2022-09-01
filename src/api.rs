1658bcd7b4828077655f37031e3d7ca3becb8dc2
Mikael Karlman <mikael.karlman@schibsted.com>
2022-09-01 19:14:49 +0200

    get travels and print them

--- src/api.rs
+++ src/api.rs
@@ -7,7 +7,52 @@ pub struct Location {
     pub name: String,
 }
 
-pub struct Travel {}
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Time {
+    pub planned: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TravelResult {
+    pub travels: Vec<Travel>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Travel {
+    pub legs: Vec<Leg>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Leg {
+    pub origin: Origin,
+    pub destination: Destination,
+    pub transport: Transport,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Origin {
+    pub name: String,
+    pub departure_time: Time,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Destination {
+    pub name: String,
+    pub arrival_time: Time,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Transport {
+    pub name: String,
+    pub direction: String,
+}
 
 pub fn search_location(search_term: &str) -> Result<Vec<Location>, reqwest::Error> {
     let body: Vec<Location> = reqwest::blocking::get(format!(
@@ -18,4 +63,15 @@ pub fn search_location(search_term: &str) -> Result<Vec<Location>, reqwest::Erro
 
     Ok(body)
 }
-//pub fn search_travels(orig_location: &Location, dest_location: &Location) -> Vec<Travel> {}
+pub fn search_travels(
+    orig_loc: &Location,
+    dest_loc: &Location,
+) -> Result<TravelResult, reqwest::Error> {
+    let body: TravelResult = reqwest::blocking::get(format!(
+        "https://webcloud.sl.se/v2/api/travels?mode=travelPlanner&origPlaceId={}&destPlaceId={}&searchForArrival=false&transportTypes=111&desiredResults=3&useCache=false",
+        orig_loc.place_id, dest_loc.place_id,
+    ))?
+    .json()?;
+
+    Ok(body)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 mod api;
 mod cli;
 
+use colored::Colorize;
+
 fn main() {
     let orig_loc = cli::ask_for_location("What station do you want to go from?");
 
     let dest_loc = cli::ask_for_location("What station do you want to go to?");
-    dbg!("orig_loc {:?}", orig_loc);
-    dbg!("dest_loc {:?}", dest_loc);
+
+    let travel_result: api::TravelResult = api::search_travels(&orig_loc, &dest_loc).expect("die");
+
+    for travel in travel_result.travels.iter() {
+        let leg_length = travel.legs.len();
+        println!(
+            "{}: {} {} {}: {}",
+            travel.legs[0].origin.name.red(),
+            travel.legs[0].origin.departure_time.planned.cyan(),
+            "=>".green(),
+            travel.legs[leg_length - 1].destination.name.red(),
+            travel.legs[leg_length - 1]
+                .destination
+                .arrival_time
+                .planned
+                .cyan()
+        );
+    }
 }
